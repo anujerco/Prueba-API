83b1e37acca84b2efc4ccd3fd8d9934cb4f5efe0
Juan Ercole <juan.m.ercole@outlook.com>
2024-03-18 19:30:51 -0300

    seed function update

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -3,6 +3,11 @@ import { data } from "./data";
 
 
 export const seed = async () => {
+
+  const books = await prisma.book.findMany();
+
+  if (books.length > 0) return;
+
   await prisma.book.createMany( {
     data: data
   } );
